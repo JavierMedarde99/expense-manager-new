a857bf4d5ee5b2a7a2792f4c39cce6e08896a844
JavierMedardeMata <32664879+JavierMedarde99@users.noreply.github.com>
2026-01-29 20:46:34 +0100

    try docker

--- src/main/java/com/expense/ui/component/MenuMain.java
+++ src/main/java/com/expense/ui/component/MenuMain.java
@@ -5,11 +5,7 @@ import java.time.LocalDate;
 import com.expense.service.BillService;
 import com.expense.ui.component.tables.TableBill;
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.contextmenu.MenuItem;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.menubar.MenuBar;
-import com.vaadin.flow.component.menubar.MenuBarVariant;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 public class MenuMain extends VerticalLayout{
@@ -17,24 +13,14 @@ public class MenuMain extends VerticalLayout{
     private Div mainContent;
 
     public MenuMain(String userName, BillService billService) {
-        MenuBar menuBar = new MenuBar();
-        menuBar.addThemeVariants(MenuBarVariant.LUMO_TERTIARY);
-        MenuItem stats = menuBar.addItem("starts");
-        MenuItem data = menuBar.addItem("data");
 
         mainContent = new Div();
         mainContent.setSizeFull();
 
-        // Acción al hacer click en stats
-        stats.addClickListener(e -> showStats(userName, billService));
+        showTables(userName, billService);
 
-        // Acción al hacer click en data (mostrar tablas)
-        data.addClickListener(e -> showTables(userName, billService));
-
-        // Inicialmente mostramos las tablas
-        showStats(userName, billService);
         
-        add(menuBar,mainContent);
+        add(mainContent);
     }
 
 
@@ -54,23 +40,5 @@ public class MenuMain extends VerticalLayout{
         lastMonth.add(titleLastMonth, tableBillLastMonth);
 
         mainContent.add(thisMonth, lastMonth);
-    }
-
-    private void showStats(String userName, BillService billService) {
-        mainContent.removeAll(); // Limpiamos contenido anterior
-
-        HorizontalLayout chartsLayout = new HorizontalLayout();
-        chartsLayout.setWidthFull();
-        chartsLayout.getStyle().set("display", "flex");
-        chartsLayout.getStyle().set("flex-wrap", "wrap");
-        chartsLayout.getStyle().set("justify-content", "space-around");
-        
-        Div pieChartThisMoth = new ChartPie(billService, userName, LocalDate.now().getMonthValue(), LocalDate.now().getYear(), "This Month");
-        pieChartThisMoth.setWidth("500px");
-        Div pieChartLastMoth = new ChartPie(billService, userName, LocalDate.now().minusMonths(1).getMonthValue(), LocalDate.now().minusMonths(1).getYear(), "Last Month");
-        pieChartLastMoth.setWidth("500px");
-
-        chartsLayout.add(pieChartThisMoth, pieChartLastMoth);
-        mainContent.add(chartsLayout);
     }
 }
